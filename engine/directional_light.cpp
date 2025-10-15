67d112d7fadc203fcaeb48bad4204bf138182225
lucamazzza <luca.mazza@student.supsi.ch>
2025-10-15 20:34:25 +0200

    feat: directional lights

---
+++ engine/directional_light.cpp
@@ -0,0 +1,49 @@
+#include "directional_light.h"
+
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "common.h"
+#include "node.h"
+
+/**
+ * Creates a directional light with default parameters.
+ * 
+ * * Direction: 0.0f, 1.0f, 0.0f (down)
+ */
+ENG_API DirectionalLight::DirectionalLight() : Light() {
+	this->direction = glm::vec3(0.0f, 1.0f, 0.0f);
+}
+
+/**
+ * Sets the light direction.
+ * It is also possible to set the direction using the node rotation.
+ * 
+ * @param direction new light direction
+ */
+void ENG_API DirectionalLight::set_direction(const glm::vec3 direction) {
+	this->direction = direction;
+}
+
+/**
+ * Renders the directional light.
+ * 
+ * This method is automatically called by the engine when rendering the scene.
+ * 
+ * @param world_matrix world transformation matrix
+ */
+void ENG_API DirectionalLight::render(const glm::mat4 world_matrix) const {
+	Node::render(world_matrix);
+	glEnable(GL_LIGHT0 + this->light_id);
+	const glm::vec4 light_pos(0.0f, 100.0f, 0.0f, 0.0f);
+	const glm::vec4 ambient(this->ambient_color, 1.0f);
+	const glm::vec4 diffuse(this->diffuse_color, 1.0f);
+	const glm::vec4 specular(this->specular_color, 1.0f);
+	const int cur_light = Light::get_current_light(this->light_id);
+	glLightfv(cur_light, GL_POSITION, glm::value_ptr(light_pos));
+	glLightfv(cur_light, GL_AMBIENT, glm::value_ptr(ambient));
+	glLightfv(cur_light, GL_DIFFUSE, glm::value_ptr(diffuse));
+	glLightfv(cur_light, GL_SPECULAR, glm::value_ptr(specular));
+}
--- engine/directional_light.h
+++ engine/directional_light.h
@@ -17,7 +17,7 @@
 class ENG_API DirectionalLight : public Light {
 public:
 	DirectionalLight();
-	void set_direction(const glm::mat4 world_matrix);
+	void set_direction(const glm::vec3 direction);
 	void render(const glm::mat4 world_matrix) const override;
 private:
 	glm::vec3 direction;

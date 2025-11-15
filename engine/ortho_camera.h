a07c1f846bdc0d71a9833f97e9146457d13277b2
Luca Mazza <luca.mazza@mazluc.ch>
2025-11-15 19:13:23 +0100

    feat(#12): ortho camera header declarations

---
+++ engine/ortho_camera.h
@@ -0,0 +1,32 @@
+/**
+ * @file	ortho_camera.h
+ * @brief	Orthographic camera class definition
+ *
+ * @author	Luca Mazza          (C) SUPSI [luca.mazza@student.supsi.ch]
+ * @author	Roeld Hoxha         (C) SUPSI [roeld.hoxha@student.supsi.ch]
+ * @author	Vasco Silva Pereira (C) SUPSI [vasco.silvapereira@student.supsi.ch]
+ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "common.h"
+#include "camera.h"
+
+namespace lrvg {
+
+/**
+ * @brief Orthographic camera class.
+ */
+class ENG_API OrthoCamera : public Camera {
+public:
+	OrthoCamera();
+	float get_zoom() const;
+    void set_zoom(float zoom);
+	void render(const glm::mat4 world_matrix) const override;
+private:
+	float zoom;
+};
+
+}

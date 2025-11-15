ff727a1b33d31b9085de8bee7c11aa867384642b
Luca Mazza <luca.mazza@mazluc.ch>
2025-11-15 19:11:12 +0100

    feat(#12): perspective camera header declarations

---
+++ engine/perspective_camera.h
@@ -0,0 +1,29 @@
+/**
+ * @file	perspective_camera.h
+ * @brief	Perspective camera class definition
+ *
+ * @author	Luca Mazza          (C) SUPSI [luca.mazza@student.supsi.ch]
+ * @author	Roeld Hoxha         (C) SUPSI [roeld.hoxha@student.supsi.ch]
+ * @author	Vasco Silva Pereira (C) SUPSI [vasco.silvapereira@student.supsi.ch]
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <glm/glm.hpp>
+
+#include "common.h"
+#include "camera.h"
+
+namespace lrvg {
+
+/**
+ * @brief Perspective camera class.
+ */
+class ENG_API PerspectiveCamera : public Camera {
+public:
+	void render(const glm::mat4 world_matrix) const override;
+};
+
+}

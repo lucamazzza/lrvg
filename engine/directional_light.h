f9a4335b1e218ec513eaa87293537798e3d326ca
Roeld <roeld.hoxha@supsi.ch.>
2025-11-20 18:20:40 +0100

    added the directional and spot light

---
+++ engine/directional_light.h
@@ -0,0 +1,28 @@
+/**
+ * @file	directional_light.h
+ * @brief	Directional light class definition
+ *
+ * @author	Luca Mazza          (C) SUPSI [luca.mazza@student.supsi.ch]
+ * @author	Roeld Hoxha         (C) SUPSI [roeld.hoxha@student.supsi.ch]
+ * @author	Vasco Silva Pereira (C) SUPSI [vasco.silvapereira@student.supsi.ch]
+ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "common.h"
+#include "light.h"
+
+namespace lrvg {
+
+	class ENG_API DirectionalLight : public Light {
+	public:
+		DirectionalLight();
+		void set_direction(const glm::vec3 direction);
+		void render(const glm::mat4 world_matrix) const override;
+	private:
+		glm::vec3 direction;
+	};
+
+}

62ede6ea212ed3ea08e331bc8d54f76c78bac7eb
vasco.silvapereira <vasco.silvapereira@supsi.ch>
2025-11-24 22:30:56 +0100

    feat: implemented texture class logic

---
+++ engine/texture.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file	texture.cpp
+ * @brief	Texture object class
+ *
+ * @author	Luca Mazza          (C) SUPSI [luca.mazza@student.supsi.ch]
+ * @author	Roeld Hoxha         (C) SUPSI [roeld.hoxha@student.supsi.ch]
+ * @author	Vasco Silva Pereira (C) SUPSI [vasco.silvapereira@student.supsi.ch]
+ */
+
+#include "texture.h"
+
+#include <glad/gl.h>
+#define FREEIMAGE_LIB
+#include <FreeImage/FreeImage.h>
+
+using namespace lrvg;
+
+ENG_API Texture::Texture(const std::string path) {
+	FIBITMAP* bmp = nullptr;
+	FREE_IMAGE_FORMAT fif = FreeImage_GetFileType(path.c_str(), 0);
+
+	if (fif == FIF_UNKNOWN)
+	{
+		fif = FreeImage_GetFIFFromFilename(path.c_str());
+	}
+	if (fif != FIF_UNKNOWN && FreeImage_FIFSupportsReading(fif))
+	{
+		bmp = FreeImage_Load(fif, path.c_str());
+	}
+
+	if (bmp == nullptr)
+	{
+		ERROR("Failed to load texture from path: %s", path.c_str());
+		this->bitmap = nullptr;
+		this->texture_id = 0;
+		return;
+	}
+	FIBITMAP* converted = FreeImage_ConvertTo32Bits(bmp);
+	FreeImage_Unload(bmp);
+	if (converted == nullptr)
+	{
+		ERROR("Failed to convert texture to 23-bit: %s", path.c_str());
+		this->bitmap = nullptr;
+		this->texture_id = 0;
+		return;
+	}
+	this->bitmap = (void*)converted;
+	const int width = FreeImage_GetWidth(converted);
+	const int height = FreeImage_GetHeight(converted);
+	BYTE* bits = FreeImage_GetBits(converted);
+	if (bits == nullptr || width == 0 || height == 0) {
+		ERROR("Invalid image data for texture: %s", path.c_str());
+		FreeImage_Unload(converted);
+		this->bitmap = nullptr;
+		this->texture_id = 0;
+		return;
+	}
+	glGenTextures(1, &this->texture_id);
+	glBindTexture(GL_TEXTURE_2D, this->texture_id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, bits);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+ENG_API Texture::~Texture() {
+	if (this->texture_id != 0) {
+		glDeleteTextures(1, &this->texture_id);
+		this->texture_id = 0;
+	}
+	if (this->bitmap != nullptr) {
+		FreeImage_Unload((FIBITMAP*)this->bitmap);
+		this->bitmap = nullptr;
+	}
+}
+
+void ENG_API Texture::render(const glm::mat4 world_matrix) const {
+	(void)world_matrix;
+	if (this->texture_id == 0) return;
+	glBindTexture(GL_TEXTURE_2D, this->texture_id);
+}
